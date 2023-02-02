1190152044a4ebf3d8acf989033a13eb9b9e3dce
Amey <Amey@DESKTOP-IQVTTVF>
2023-02-02 14:13:56 +0530

    Second day first commit

---
+++ daytwo/src/org/tnsif/operators/OperatorDemo.java
@@ -0,0 +1,27 @@
+package org.tnsif.operators;
+
+import java.util.Scanner;
+
+public class OperatorDemo {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		int y = s.nextInt();
+		// Arithmatic 
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+		
+		//Assignment
+		//Logical
+		//Boolean
+		//Bitwise
+		//Ternary
+		//Unary
+
+	}
+
+}
