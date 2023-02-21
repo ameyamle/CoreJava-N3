8c7bf65dec7c8920e6985821cc7647d337896a33
Amey <Amey@DESKTOP-IQVTTVF>
2023-02-21 12:55:13 +0530

    Day Eleven

---
+++ dayeleven/src/org/tnsif/annotationsdemo/SupressWarningDemo.java
@@ -0,0 +1,16 @@
+package org.tnsif.annotationsdemo;
+
+import java.util.Scanner;
+public class SupressWarningDemo {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		// use to remove warnings
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		System.out.println(x);
+		
+
+	}
+
+}
