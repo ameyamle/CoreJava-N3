8694b15a472080e0b79b7a21afa71354e2646297
ameyamle <91939327+ameyamle@users.noreply.github.com>
2023-03-06 13:17:33 +0530

    Delete dayten directory

--- dayten/src/org/tnsif/threaddemo/SimpleThreadProgram.java
+++
@@ -1,9 +0,0 @@
-package org.tnsif.threaddemo;
-//program to demonstrate on thread class
-//creating thread class by extending thread class
-public class SimpleThreadProgram extends Thread {
-	
-	public void run() {
-		System.out.println("Thread is in running state");
-	}
-}
